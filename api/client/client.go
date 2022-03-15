6f11cabe4dd2e45cd1b80d5bd050c00c4c62e978
kenji <kenjix12x22@gmail.com>
2022-03-15 16:47:49 +0900

    react側からapiからapiを叩くまで実装

--- api/client/client.go
+++ api/client/client.go
@@ -1,9 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+	
 
 func main() {
     r := gin.Default()
+		r.Use(cors.New(cors.Config{
+			// 許可したいHTTPメソッドの一覧
+			AllowMethods: []string{
+					"POST",
+					"GET",
+					"OPTIONS",
+					"PUT",
+					"DELETE",
+			},
+			// 許可したいHTTPリクエストヘッダの一覧
+			AllowHeaders: []string{
+					"Access-Control-Allow-Headers",
+					"Content-Type",
+					"Content-Length",
+					"Accept-Encoding",
+					"X-CSRF-Token",
+					"Authorization",
+			},
+			// 許可したいアクセス元の一覧
+			AllowOrigins: []string{
+					"http://localhost:3000",
+			},
+			// 自分で許可するしないの処理を書きたい場合は、以下のように書くこともできる
+			// AllowOriginFunc: func(origin string) bool {
+			//  return origin == "https://www.example.com:8080"
+			// },
+			// preflight requestで許可した後の接続可能時間
+			// https://godoc.org/github.com/gin-contrib/cors#Config の中のコメントに詳細あり
+			MaxAge: 24 * time.Hour,
+	}))
+
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState<string>()
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/ping', { headers: { 'Content-Type': 'application/json' } })
+    .then(res => {
+      setMessage(res.data.message)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{message}</p>
     </div>
   );
 }
