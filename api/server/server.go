7af1d046a7e3966d75940cfcef46b9da507a3b21
kenji <kenjix12x22@gmail.com>
2022-03-17 23:10:54 +0900

    server.goの雛形作成

--- api/server/server.go
+++ api/server/server.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"todo/pb"
+	"todo/server/db"
+
+	"google.golang.org/grpc"
+)
+
+func checkErr(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	db.DbConnect()
+}
+
+type server struct {}
 
 func main() {
-	fmt.Println("aaa")
+	fmt.Println("起動")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	checkErr("Failed to listen: %v", err)
+
+	s := grpc.NewServer()
+	pb.RegisterAppServiceServer(s, &server{})
+	s.Serve(lis)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+
+	fmt.Println("終了中")
+	s.Stop()
+	lis.Close()
+	fmt.Println("終了")
 }
+
