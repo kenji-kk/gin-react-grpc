bc835eb6eb43af44e954bbfa78d242197c7a0d26
kenji <kenjix12x22@gmail.com>
2022-03-20 12:22:51 +0900

    mui/stylesインストール

--- front/src/components/pages/Home.tsx
+++ front/src/components/pages/Home.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { makeStyles } from '@mui/styles';
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles({
+  root: {
+
+  }
+})
 
 export const Home = () => {
+  const classes = useStyles();
+
   return (
     <div>Home</div>
   )
