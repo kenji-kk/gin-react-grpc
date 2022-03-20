eb084dc3169b0858c3e8e58f2e13118a9d247036
kenji <kenjix12x22@gmail.com>
2022-03-20 13:01:36 +0900

    タスク追加ボタン作成

--- front/src/components/pages/Home.tsx
+++ front/src/components/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { makeStyles } from '@mui/styles';
-import Container from "@material-ui/core/Container";
+import { Container } from '@mui/material';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 const useStyles = makeStyles({
-  root: {
-
+  title: {
+    textAlign: 'center',
+  },
+  buttonWrap: {
+    textAlign: 'center',
   }
 })
 
@@ -12,6 +17,14 @@ export const Home = () => {
   const classes = useStyles();
 
   return (
-    <div>Home</div>
+    <Container>
+      <h1 className={classes.title}>TODOアプリ</h1>
+      <div className={classes.buttonWrap}>
+        <Button  variant="contained" endIcon={<AddIcon />}>
+          タスク追加
+        </Button>
+      </div>
+    </Container>
+   
   )
 }
