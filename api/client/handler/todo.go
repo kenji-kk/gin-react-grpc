526c769ef6f8ad4a98f33ba858801f050c6be067
kenji <kenjix12x22@gmail.com>
2022-03-21 12:32:32 +0900

    client側のCreateTodoハンドラ途中まで

--- api/client/handler/todo.go
+++ api/client/handler/todo.go
@@ -1,7 +1,28 @@
 package handler
 
-type Todo struct {
-	Id int64
-	Title string `binding:"required,min=1,max=50"`
-	Content string `binding:"required,min=1,max=50"`
+import (
+	"net/http"
+	"todo/client/jwt"
+	"todo/client/lib"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTodo(ctx *gin.Context){
+	todo := new(lib.Todo)
+	if err := ctx.Bind(todo); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+
+	user, err := jwt.CurrentUser(ctx)
+  if err != nil {
+    ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+    return
+  }
+
+	if err := store.AddPost(user, post); err != nil {
+    ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
 }
