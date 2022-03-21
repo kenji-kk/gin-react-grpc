614290db8b6e30daeeef1cf214cd9f6b23cd6309
kenji <kenjix12x22@gmail.com>
2022-03-21 13:51:35 +0900

    client側のCreateTodo作成

--- api/client/handler/todo.go
+++ api/client/handler/todo.go
@@ -1,14 +1,24 @@
 package handler
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"todo/client/jwt"
 	"todo/client/lib"
+	"todo/pb"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 func CreateTodo(ctx *gin.Context){
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("server:50051", opts)
+	CheckErr("could not connect: %v\n", err)
+	defer cc.Close()
+	c := pb.NewTodoServiceClient(cc)
+
 	todo := new(lib.Todo)
 	if err := ctx.Bind(todo); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
@@ -21,8 +31,26 @@ func CreateTodo(ctx *gin.Context){
     return
   }
 
-	if err := store.AddPost(user, post); err != nil {
-    ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	pUser := &pb.User{
+		Id: user.Id,
+		UserName: user.UserName,
+		Email: user.Email,
+	}
+
+	pTodo := &pb.Todo{
+		Title: todo.Title,
+		Content: todo.Content,
+	}
+
+	_, err = c.CreateTodo(context.Background(), &pb.CreateTodoRequest{User: pUser, Todo: pTodo})
+	if err != nil {
+    fmt.Printf("LoginUserClientでエラーがありました: %v\n", err)
+    ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
     return
   }
+
+	ctx.JSON(http.StatusOK, gin.H{
+    "msg":  "Post created successfully.",
+    "data": todo,
+  })
 }
