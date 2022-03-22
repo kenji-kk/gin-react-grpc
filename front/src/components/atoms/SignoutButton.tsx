69c8b0f1ff4a92fa3eec71b0d9deaf611014410a
kenji <kenjix12x22@gmail.com>
2022-03-23 00:26:02 +0900

    signoutボタン機能実装

--- front/src/components/atoms/SignoutButton.tsx
+++ front/src/components/atoms/SignoutButton.tsx
@@ -1,10 +1,25 @@
-import { memo } from 'react'
+import { memo, useContext } from 'react'
+import { AuthContext } from "../../App"
+import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom"
 
 import Button from '@mui/material/Button';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
-export const SignoutButton = memo(() => {
+export const SignoutButton:React.VFC = memo(() => {
+  const authContext = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    authContext.setIsSignedIn(false);
+    authContext.setJwt(undefined);
+    Cookies.remove("_access_token");
+    navigate('/');
+  }
+
+
   return (
-    <Button color="secondary">ログアウト<ExitToAppIcon sx={{ fontSize: 40 }}></ExitToAppIcon></Button>
+    <Button onClick={handleClick} color="secondary">ログアウト<ExitToAppIcon sx={{ fontSize: 40 }}></ExitToAppIcon></Button>
   )
 })
