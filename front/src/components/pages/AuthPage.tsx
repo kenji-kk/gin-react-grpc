95d25ee2daa472b981fd9406ce8c7a2c52f626a3
kenji <kenjix12x22@gmail.com>
2022-03-23 00:38:44 +0900

    authページにタイトル付与

--- front/src/components/pages/AuthPage.tsx
+++ front/src/components/pages/AuthPage.tsx
@@ -1,13 +1,21 @@
 import { useState, memo } from 'react'
+import { makeStyles } from '@mui/styles';
 import { SignUpForms } from '../organisms/SignupForms'
 import { SignInForms } from '../organisms/SigninForms'
 
+const useStyles = makeStyles({
+  title: {
+    textAlign: 'center',
+  },
+})
+
 export const AuthPage:React.VFC = memo(() => {
   const [formToggle, setFormToggle] = useState(true)
-
+  const classes = useStyles();
 
   return (
     <>
+      <h1 className={classes.title}>TODOアプリ</h1>
       {formToggle ? <SignUpForms setFormToggle={setFormToggle}/> : <SignInForms setFormToggle={setFormToggle}/>}
     </>
   )
